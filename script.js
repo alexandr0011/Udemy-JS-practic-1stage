40f67466e4bdbe35aeb3408c0efbeba5c76b983e
Alexandr <alexandrpo271@gmail.com>
2022-01-19 19:54:53 +0300

    Second practic lession

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
@@ -8,14 +22,58 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a =  prompt("Один из просмотренных фильмов?", "");
-const b =  prompt("На сколько его оцените?", "");
-const c =  prompt("Один из просмотренных фильмов?", "");
-const d =  prompt("На сколько его оцените?", "");
+for (let i = 0; i < 2; i++) {
+    const a =  prompt("Один из просмотренных фильмов?", "");
+    const b =  prompt("На сколько его оцените?", "");
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// let i = 0;
+
+// while (i < 2) {
+//     const a =  prompt("Один из просмотренных фильмов?", "");
+//     const b =  prompt("На сколько его оцените?", "");
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         i++;
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// }
+
+// do {
+//     const a =  prompt("Один из просмотренных фильмов?", "");
+//     const b =  prompt("На сколько его оцените?", "");
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         i++;
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// }
+// while (i < 2);
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотренно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
-
-console.log(1);
