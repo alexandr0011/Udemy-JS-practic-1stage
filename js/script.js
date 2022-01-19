fb0a4c437d56971919b0570c4e25ded53c912e62
Alexandr <alexandrpo271@gmail.com>
2022-01-19 20:49:26 +0300

    Six practic lession

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -28,23 +31,72 @@ const adv = document.querySelectorAll('.promo__adv img');
 const genre = document.querySelector('.promo__genre');
 const bg = document.querySelector('.promo__bg');
 const filmsList = document.querySelector('.promo__interactive-list');
+const addForm = document.querySelector('form.add');
+const addInput = addForm.querySelector('.adding__input');
+const checkbox = addForm.querySelector('[type="checkbox"]');
 
-adv.forEach(item => {
-    item.remove();
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let newFilm = addInput.value;
+    const favorite = checkbox.checked;
+    if (newFilm) {
+
+        if (newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`;
+        }
+
+        if (favorite) {
+            console.log("Добавляем любимый фильм");
+        }
+        movieDB.movies.push(newFilm);
+        sortArr(movieDB.movies);
+
+        createMovieList(movieDB.movies, filmsList);
+    }
+    
+    event.target.reset();
+
 });
 
-genre.innerHTML = "ДРАМА";
+const deleteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
 
-bg.style.backgroundImage = "url('../img/bg.jpg')";
+const makeChanges = () => {
+    genre.innerHTML = "ДРАМА";
+    bg.style.backgroundImage = "url('../img/bg.jpg')";
+};
 
-filmsList.innerHTML = '';
-movieDB.movies.sort();
+const sortArr = (arr) => {
+    arr.sort();
+};
 
-movieDB.movies.forEach((film, index) => {
-    filmsList.innerHTML += `
-            <li class="promo__interactive-item">${index + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
 
+
+function createMovieList(films, parent) {
+    parent.innerHTML = '';
+    sortArr(films);
+    films.forEach((film, index) => {
+        parent.innerHTML += `
+                <li class="promo__interactive-item">${index + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+
+            createMovieList(films, parent);
+        });
+    });
+}
+
+createMovieList(movieDB.movies, filmsList);
+deleteAdv(adv);
+makeChanges();
